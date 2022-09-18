467daa5ccbf3244bbfb1c06f5cdde97eaa2ae3df
URAS <URAS@DESKTOP-U3BG89S>
2022-09-18 20:47:52 +0300

    datatypes

---
+++ hafta1/src/dataTypes/Main.java
@@ -0,0 +1,23 @@
+package dataTypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int intSayi;
+		intSayi=2147483647;
+		
+		byte byteSayi;
+		byteSayi=127;
+		
+		double doubleSayi;
+		doubleSayi=15.55;
+		
+		char charKarakter;
+		charKarakter='c';
+		
+		boolean boolTip;
+		boolTip=true;
+
+	}
+
+}
