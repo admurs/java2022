5a468de7a5b1af109d4bb3cfd449d70a7166fc5c
URAS <URAS@DESKTOP-U3BG89S>
2022-09-18 20:37:12 +0300

    variables

---
+++ hafta1/src/variables/Main.java
@@ -0,0 +1,11 @@
+package variables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int ogrenciSayısı=12;
+		String mesaj="Öğrenci sayısı : ";
+		System.out.println(mesaj+ogrenciSayısı);
+	}
+
+}
