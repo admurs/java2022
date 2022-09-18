d92034a06c0fb02b67eb3cbb7b555a798b655ec4
URAS <URAS@DESKTOP-U3BG89S>
2022-09-18 21:22:02 +0300

    loopdemo

---
+++ hafta1/src/loopDemo/Main.java
@@ -0,0 +1,23 @@
+package loopDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		for (int i = 1; i < 10; i++) {
+			System.out.println(i);
+		}
+		System.out.println("Döngü bitti");
+		
+		for (int i = 1; i < 10; i+=2) {
+			System.out.println(i);
+		}
+		System.out.println("Tek sayılar bitti");
+		
+		for (int i = 2; i < 10; i+=2) {
+			System.out.println(i);
+		}
+		System.out.println("Çift sayılar bitti");
+		
+	}
+
+}
