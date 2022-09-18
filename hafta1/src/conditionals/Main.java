654d7677b2ba1ea3a40e91f689ac4c9848067d52
URAS <URAS@DESKTOP-U3BG89S>
2022-09-18 20:52:44 +0300

    conditionals

---
+++ hafta1/src/conditionals/Main.java
@@ -0,0 +1,19 @@
+package conditionals;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int sayi=20;
+		if (sayi>20) {
+			System.out.println("Sayi 20'den büyük");
+		}
+		else if(sayi<20){
+			System.out.println("Sayi 20'den kücük");
+		}
+		else {
+			System.out.println("Sayi 20'ye eşit");
+		}
+
+	}
+
+}
