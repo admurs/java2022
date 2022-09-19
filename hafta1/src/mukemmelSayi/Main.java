3a9afcecbec808afafecf05d3e6cc965eba03080
URAS <URAS@DESKTOP-U3BG89S>
2022-09-19 22:08:45 +0300

    mukemmelsayi

---
+++ hafta1/src/mukemmelSayi/Main.java
@@ -0,0 +1,22 @@
+package mukemmelSayi;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int sayi=28;
+		int toplam=0;
+		
+		for (int i = 1; i < sayi; i++) {
+			if (sayi%i==0) {
+				toplam=toplam+i;
+			}
+		}
+		if (toplam==sayi) {
+			System.out.println(sayi+" : sayısı mükemmel sayıdır.");
+		}else {
+			System.out.println(sayi+" : sayısı mükemmel değildir.");
+		}
+
+	}
+
+}
