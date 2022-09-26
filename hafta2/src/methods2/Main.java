f83a88b85e9a7bb08950eac3799100f97683b540
URAS <URAS@DESKTOP-U3BG89S>
2022-09-26 23:01:15 +0300

    methods2

--- hafta2/src/methods2/Main.java
+++ hafta2/src/methods2/Main.java
@@ -8,6 +8,8 @@ public class Main {
 		System.out.println(yeniMesaj);
 		int sayi=topla(7,5);
 		System.out.println(sayi);
+		int toplam = toplam(1,3,5,7,9);
+		System.out.println(toplam);
 
 	}
 	
@@ -26,6 +28,15 @@ public static void guncelle() {
 public static int topla(int sayi1,int sayi2) {
 	return sayi1+sayi2;
 }
+
+public static int toplam(int... sayilar) {
+	int toplam=0;
+	for (int i : sayilar) {
+		toplam=toplam+i;
+	}
+	return toplam;
+}
+
 public static String sehirVer() {
 	return "Ankara";
 }
